2b00fe3baaaa840358c670db9383ecec0e244d23
Jessie Noel Lapure <lapurejessie@gmail.com>
2025-11-03 21:44:55 +0800

    chore: Add CORS configuration for local development

--- backend/src/main/java/com/digihealth/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/digihealth/backend/config/SecurityConfig.java
@@ -26,6 +26,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import org.springframework.web.filter.CorsFilter;
+
+
+
+import java.util.Arrays;
+
 
 
 @Configuration
@@ -64,6 +74,30 @@ public class SecurityConfig {
 
     @Bean
 
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.addAllowedOrigin("http://localhost:3000");
+
+        config.addAllowedHeader("*");
+
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+
+    }
+
+
+
+    @Bean
+
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.cors().and().csrf().disable()
@@ -89,3 +123,5 @@ public class SecurityConfig {
     }
 
 }
+
+
