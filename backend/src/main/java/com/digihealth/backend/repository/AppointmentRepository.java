422760813821e408160bf5d1a386d76151e085ad
Jessie Noel Lapure <lapurejessie@gmail.com>
2025-11-12 12:18:51 +0800

    feat(BE): Update repository layer and remove unused files

--- backend/src/main/java/com/digihealth/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/digihealth/backend/repository/AppointmentRepository.java
@@ -1,11 +1,16 @@
 package com.digihealth.backend.repository;
 
 import com.digihealth.backend.entity.Appointment;
+import com.digihealth.backend.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+    List<Appointment> findByDoctorAndAppointmentDate(Doctor doctor, LocalDate date);
+    List<Appointment> findByDoctor(Doctor doctor);
 }
--- backend/src/main/java/com/digihealth/backend/repository/DoctorRepository.java
+++ backend/src/main/java/com/digihealth/backend/repository/DoctorRepository.java
@@ -1,11 +1,14 @@
 package com.digihealth.backend.repository;
 
 import com.digihealth.backend.entity.Doctor;
+import com.digihealth.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, UUID> {
+    Optional<Doctor> findByUser(User user);
 }
