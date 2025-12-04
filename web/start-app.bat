207fd2b14d963a6bc60f0555fbad688711b2c165
Jessie Noel Lapure <lapurejessie@gmail.com>
2025-12-04 20:48:16 +0800

    chore(web): set DISABLE_ESLINT_PLUGIN in start-app.bat to bypass CRA jest env lint error

--- web/start-app.bat
+++ web/start-app.bat
@@ -3,4 +3,5 @@ echo Starting DigiHealth Frontend...
 echo.
 cd /d "%~dp0"
 set NODE_OPTIONS=--openssl-legacy-provider
+set DISABLE_ESLINT_PLUGIN=true
 node node_modules/react-scripts/scripts/start.js %*
